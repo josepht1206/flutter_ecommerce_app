9bf456c94d5b6569950a4746dfadf5a028fee5bb
Joseph.T <joseph.tan@hexainitiative.com>
2023-06-04 14:19:18 +0800

    [] Implement shared preferences for cart persistence

--- lib/models/cart_item.dart
+++ lib/models/cart_item.dart
@@ -2,5 +2,22 @@ class CartItem {
   final dynamic product;
   int quantity;
 
-  CartItem({required this.product, this.quantity = 1});
+  CartItem({
+    required this.product,
+    this.quantity = 1,
+  });
+
+  factory CartItem.fromJson(Map<String, dynamic> json) {
+    return CartItem(
+      product: json['product'],
+      quantity: json['quantity'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'product': product,
+      'quantity': quantity,
+    };
+  }
 }
--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,54 +1,81 @@
-import 'package:flutter/foundation.dart';
-import 'package:ecommerce_app/models/cart_item.dart';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/cart_item.dart';
 
 class CartProvider extends ChangeNotifier {
+  static const _cartKey = 'cart';
+
   List<CartItem> _cartItems = [];
+  SharedPreferences? _sharedPreferences;
 
   List<CartItem> get cartItems => _cartItems;
 
-  int get cartItemCount => _cartItems.length;
+  CartProvider() {
+    _initCart();
+  }
+
+  Future<void> _initCart() async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+    final savedCart = _sharedPreferences!.getString(_cartKey);
+    if (savedCart != null) {
+      final List<dynamic> cartData = jsonDecode(savedCart);
+      _cartItems = cartData.map((item) => CartItem.fromJson(item)).toList();
+      notifyListeners();
+    }
+  }
+
+  void _saveCart() {
+    final cartData = _cartItems.map((item) => item.toJson()).toList();
+    _sharedPreferences!.setString(_cartKey, jsonEncode(cartData));
+  }
 
   void addToCart(CartItem item) {
-    // Check if the product is already in the cart
     final existingItemIndex = _cartItems.indexWhere(
       (cartItem) => cartItem.product['id'] == item.product['id'],
     );
 
     if (existingItemIndex != -1) {
-      // Increase the quantity if the product is already in the cart
       _cartItems[existingItemIndex].quantity++;
     } else {
-      // Add the product to the cart
       _cartItems.add(item);
     }
 
+    _saveCart();
     notifyListeners();
   }
 
-  void removeFromCart(CartItem item) {
+  void removeCartItem(CartItem item) {
     _cartItems.remove(item);
+    _saveCart();
+    notifyListeners();
+  }
+
+  void clearCart() {
+    _cartItems.clear();
+    _saveCart();
     notifyListeners();
   }
 
   void decreaseQuantity(CartItem item) {
     if (item.quantity > 1) {
       item.quantity--;
-      notifyListeners();
     }
+    _saveCart();
+    notifyListeners();
   }
 
   void increaseQuantity(CartItem item) {
     item.quantity++;
-    notifyListeners();
-  }
-
-  void clearCart() {
-    _cartItems.clear();
+    _saveCart();
     notifyListeners();
   }
 
   void removeItem(CartItem item) {
     _cartItems.remove(item);
+    _saveCart();
     notifyListeners();
   }
 }
